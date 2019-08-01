e39030c656f08aba95ad4eb038a65151467c0412
Will Hodgson <will.hodgson@current-rms.com>
2019-08-01 12:25:18 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand if BRANDS.none? {|b| b == brand}
+    BRANDS << brand if BRANDS.none? {|b| b == brand }
   end
 
   def cobble
